a9698f8831708c4d0256349e24e651e2b6b2166d
madhuridudhane <madhuri.dudhane123@gmail.com>
2024-02-09 23:52:31 +0530

    added

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -11,3 +11,12 @@ test("verify the login functionality with valid credentials",async({page})=>{
      await expect(page.locator('h6[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]')).toHaveText('Dashboard')
 
 })
+
+test("verify the login functionality with invalid credentials",async({page})=>{
+     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+     await page.locator('input[name="username"]').fill('Admin')
+     await page.locator('input[name="password"]').fill('admin12')
+     await page.locator('button[type="submit"]').click()
+     await page.waitForTimeout(4000)
+     await expect(page.locator('p[class="oxd-text oxd-text--p oxd-alert-content-text"]')).toBeVisible()
+})
